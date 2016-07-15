c8ce71730efceb632e86baca3aebb768c3b8637b
skaxwng <samkj.ks@gmail.com>
2016-07-15 14:50:53 -0500

    Removed apollo-tools

--- imports/server/main.js
+++ imports/server/main.js
@@ -1,4 +1,4 @@
-import { configureGraphQLServer } from 'apollo-tools';
+import configureGraphQLServer from './configs/configure-server';
 import schema from './data/schema';
 import resolvers from './data/resolvers';
 import seed from './configs/seed';
